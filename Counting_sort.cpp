a6065af97d494c6902a36cb2c396502aa9a866e1
Atiqur Rahman <mdatikullah88@gmail.com>
2019-03-05 20:25:23 +0600

    Counting _sort (with negative number)

--- Counting_sort.cpp
+++ Counting_sort.cpp
@@ -1,41 +1,47 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int digit[10];
-
- void Counting_sort(int arr[],int n){
-    int temp[n+1];
-    for(int i=0;i<n;i++){
-        int temp;
-        temp=arr[i];
-        digit[temp]+=1;
-    }
-
-    //  Cumulative sum //
-
-    for(int i=0;i<10-1;i++){
-        digit[i+1]+=digit[i];
-    }
-
-    for(int i=0;i<n;i++){
-        int t1,t2;
-        t1=arr[i];
-        t2=digit[t1];
-        temp[t2]=arr[i];
-        digit[t1]-=1;
-    }
-
-    for (int i=1;i<=n;i++) {
-        cout << temp[i] << " ";
+void print(int arr[],int n){
+    for (int i=0;i<n;i++) {
+        cout << arr[i] << " " ;
     }
     cout << endl;
+}
+
+void Counting_sort(int arr[],int n){
+    int max=*max_element(arr,arr+n);
+    int min=*min_element(arr,arr+n);
+    int range=max-min+1;
+
+    int index[range]={0};
+    int output[n];
+
+    for (int i=0;i<n;i++) {
+        index[arr[i]-(min)]++;
+
+    }
+
+    for (int i=1;i<range;i++) {
+        index[i]+=index[i-1];
+    }
+
+    for (int i=0;i<n;i++){
+        output[index[arr[i]-min]-1]=arr[i];
+        index[arr[i]-min]--;
+    }
+
+    for (int i=0;i<n;i++) {
+        arr[i]=output[i];
+    }
 
 }
 
-int main() {
-    int arr[]={2,4,7,5,8,9,5,8,3,2,8,9,6,0};
-    int n=sizeof (arr) / sizeof (arr[0]);
+int main () {
+    int arr[]={-5,-10,0,-4,9,6,-1,10};
+    int n=sizeof(arr)/sizeof(arr[0]);
     Counting_sort(arr,n);
+    print(arr,n);
     return 0;
 }
